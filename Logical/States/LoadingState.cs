0b985050f8fe68b0ec7a9cad32c35cf8d174620b
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-03-25 17:05:34 -0300

    refactor: LoadingState

--- Logical/States/LoadingState.cs
+++ Logical/States/LoadingState.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Logical.States;
 
@@ -15,96 +15,97 @@ public class LoadingState : GameState
     public LoadingState() // Make it! 0
     {
         Statics.Lives = 3;
-        message = "MAKE IT!";
-        mode = 0;
+        _message = "MAKE IT!";
+        _mode = Mode.Start;
         LevelTextures.LoadTextures();
     }
     public LoadingState(string deathReason) // You failed! 1
     {
         if (Configs.Lives != 0)
             Configs.Lives--;
-        message = "YOU FAILED!";
-        mode = 1;
-        displayMessages.Add(deathReason);
+        _message = "YOU FAILED!";
+        _mode = Mode.Failed;
+        _displayMessages.Add(deathReason);
     }
-    public LoadingState(int timeLeft, int ballsLeft, int colorJobs, bool SUPERBONUS = false) // You made it! 2
+    public LoadingState(int timeLeft, int ballsLeft, int colorJobs, bool superbonus = false) // You made it! 2
     {
-        message = "YOU MADE IT!";
-        mode = 2;
-        int points = 0;
-        displayMessages.Add($"TIME BONUS: 10*{timeLeft}%");
-        bonuses.Add(10 * timeLeft);
-        if (ballsLeft is 0)
+        _message = "YOU MADE IT!";
+        _mode = Mode.Complete;
+        _displayMessages.Add($"TIME BONUS: 10*{timeLeft}%");
+        _bonuses.Add(10 * timeLeft);
+        switch (ballsLeft)
         {
-            displayMessages.Add("PERFECT BONUS:");
-            bonuses.Add(5000);
-        }
-        else if (ballsLeft is 1)
-        {
-            displayMessages.Add("SEMIPERFECT BONUS:");
-            bonuses.Add(2000);
-        }
-        else if (ballsLeft < 10)
-        {
-            displayMessages.Add($"BALL BONUS: 1000-10*{ballsLeft}");
-            bonuses.Add(1000-10*ballsLeft);
-        }
-        else
-        {
-            displayMessages.Add("BALL BONUS:");
-            bonuses.Add(0);
+            case 0:
+                _displayMessages.Add("PERFECT BONUS:");
+                _bonuses.Add(5000);
+                break;
+            case 1:
+                _displayMessages.Add("SEMIPERFECT BONUS:");
+                _bonuses.Add(2000);
+                break;
+            case < 10:
+                _displayMessages.Add($"BALL BONUS: 1000-10*{ballsLeft}");
+                _bonuses.Add(1000-10*ballsLeft);
+                break;
+            default:
+                _displayMessages.Add("BALL BONUS:");
+                _bonuses.Add(0);
+                break;
         }
 
-        if (colorJobs is not 0)
-            displayMessages.Add($"COLOR JOBS: 500*{colorJobs}");
-        else
-            displayMessages.Add("COLOR JOBS:");
-        bonuses.Add(500*colorJobs);
+        _displayMessages.Add("COLOR JOBS:" + (colorJobs is not 0 ? $" 500*{colorJobs}" : ""));
+        _bonuses.Add(500*colorJobs);
 
-        if (SUPERBONUS)
+        if (superbonus)
         {
-            displayMessages.Add("S U P E R B O N U S");
-            bonuses.Add(10000);
+            _displayMessages.Add("S U P E R B O N U S");
+            _bonuses.Add(10000);
         }
 
-        foreach (int i in bonuses)
-            points += i;
+        int points = _bonuses.Sum();
 
-        displayMessages.Add("POINTS:");
-        bonuses.Add(points);
+        _displayMessages.Add("POINTS:");
+        _bonuses.Add(points);
         Configs.Score += points;
     }
     #endregion
 
     #region Fields
-    private int mode;
-    private string message;
-    private TextComponent changingDisplay;
-    private List<string> displayMessages = new List<string>();
-    private TextComponent bonusMessage;
-    private List<int> bonuses = new List<int>();
+    private readonly Mode _mode;
+    private readonly string _message;
+    private TextComponent _changingDisplay;
+    private readonly List<string> _displayMessages = new();
+    private TextComponent _bonusMessage;
+    private readonly List<int> _bonuses = new();
+
+    private enum Mode
+    {
+        Start,
+        Failed,
+        Complete
+    }
     #endregion
 
     #region Default Methods
-    public override void LoadContent(ContentManager Content)
+    public override void LoadContent(ContentManager content)
     {
         Statics.ShowCursor = false;
-        AddGameObject(new SimpleImage(Content.Load<Texture2D>(@$"{Configs.GraphicSet}\Loading"), new Vector2(0, 28), 0));
-        AddGameObject(new TextComponent(Statics.LightFont, new Vector2(84, 43), Color.White, message, 1, TextComponent.Alignment.Center));
-        Texture2D pinkBall = Content.Load<Texture2D>("SpinnerBallPink");
+        AddGameObject(new SimpleImage(content.Load<Texture2D>($"{Configs.GraphicSet}/Loading"), new Vector2(0, 28), 0));
+        AddGameObject(new TextComponent(Statics.LightFont, new Vector2(84, 43), Color.White, _message, 1, TextComponent.Alignment.Center));
+        var pinkBall = content.Load<Texture2D>("SpinnerBallPink");
         for (int i = 0; i < Configs.Lives; i++)
             AddGameObject(new SimpleImage(pinkBall, new Vector2(72 + 12 * i, 82), 1));
         AddGameObject(new TextComponent(Statics.LightFont, new Vector2(16, 123), Color.White, $"{Configs.Stage:00} {/*Statics.LevelPassword*/ new Lexer().GetLevelName(Configs.Stage)}", 1));
-        if (mode is not 0)
+        if (_mode is not Mode.Start)
         {
             // TODO: Implement changingDisplay with displayMessages
-            changingDisplay = new TextComponent(Statics.LightFont, new Vector2(24, 163), Color.White, displayMessages[0], 1);
-            AddGameObject(changingDisplay);
-            if (mode is 2)
+            _changingDisplay = new TextComponent(Statics.LightFont, new Vector2(24, 163), Color.White, _displayMessages[0], 1);
+            AddGameObject(_changingDisplay);
+            if (_mode is Mode.Complete)
             {
                 // TODO: Implement bonusMessag with bonuses
-                bonusMessage = new TextComponent(Statics.LightFont, new Vector2(232, 163), Color.White, $"= {bonuses[0]:000000}", 1);
-                AddGameObject(bonusMessage);
+                _bonusMessage = new TextComponent(Statics.LightFont, new Vector2(232, 163), Color.White, $"= {_bonuses[0]:000000}", 1);
+                AddGameObject(_bonusMessage);
             }
         }
         AddGameObject(new TextComponent(Statics.BoldFont/*DEBUG TextureFont*/, new Vector2(209, 188), Color.White, $"{Configs.Score:000000}", 1));
@@ -119,21 +120,18 @@ public class LoadingState : GameState
                 SwitchState(new MenuState());
                 });
         else
-            FadeOut(() => Exit());
+            FadeOut(Exit);
     }
 
     public override void HandleInput(object s, ButtonEventArgs e)
     {
         if (e.Button is "LeftButton" or "RightButton")
-        {
-            FadeOut(() => Exit());
-        }
+            FadeOut(Exit);
     }
 
-    public override void UnloadContent(ContentManager Content)
-    {
-        Content.UnloadAsset((@$"{Configs.GraphicSet}\Loading"));
-    }
+    public override void UnloadContent(ContentManager content)
+        => content.UnloadAsset($"{Configs.GraphicSet}/Loading");
+
     #endregion
 
     #region Custom Methods
@@ -160,10 +158,10 @@ public class LoadingState : GameState
     }
     private void Exit()
     {
-        switch (mode)
+        switch (_mode)
         {
-            case 0: SwitchState(new LevelState()); break;
-            case 1:
+            case Mode.Start: SwitchState(new LevelState()); break;
+            case Mode.Failed:
                 if (Statics.Lives == 0)
                 {
                     Configs.Score = 0;
@@ -173,7 +171,7 @@ public class LoadingState : GameState
                 else
                     SwitchState(new LevelState());
                 break;
-            case 2: SwitchState(new LevelState()); break;
+            case Mode.Complete: SwitchState(new LevelState()); break;
             default: throw new NotImplementedException();
         }
     }
