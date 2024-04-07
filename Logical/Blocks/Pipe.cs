157fbf34c84a38013e1c44e9c551646664f51ff3
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-04-06 21:06:48 -0300

    fix(Pipe): values for inheriting blocks

--- Logical/Blocks/Pipe.cs
+++ Logical/Blocks/Pipe.cs
@@ -9,11 +9,11 @@ public class Pipe : Block
     public Pipe(Game game, Point arrayPosition, byte xx, byte yy, bool randomize = true) 
         : base(game, TextureSwitcher(xx, randomize), arrayPosition, xx, yy) { }
 
-    private static Texture2D TextureSwitcher(byte xx, bool randomize) => xx switch
+    private static Texture2D TextureSwitcher(byte xx, bool randomize) => (xx % 3) switch
     {
-        0x02 => !randomize || Statics.Brandom.Next(0, 2) == 0 ? LevelResources.PipeHorizontal : LevelResources.PipeHorizontalAlt,
-        0x03 => !randomize || Statics.Brandom.Next(0, 2) == 0 ? LevelResources.PipeVertical : LevelResources.PipeVerticalAlt,
-        0x04 => LevelResources.PipeCross,
+        2 => !randomize || Statics.Brandom.Next(0, 2) == 0 ? LevelResources.PipeHorizontal : LevelResources.PipeHorizontalAlt,
+        0 => !randomize || Statics.Brandom.Next(0, 2) == 0 ? LevelResources.PipeVertical : LevelResources.PipeVerticalAlt,
+        1 => LevelResources.PipeCross,
         _ => throw new ArgumentException("Invalid Pipe direction")
     };
 }
