e45a025ef1e3405ee046f68f29fcd5d5486d8f51
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-04-14 00:23:28 -0300

    refactor(Statics): SpinnerBallExploded

--- Logical/Statics.cs
+++ Logical/Statics.cs
@@ -171,7 +171,6 @@ public enum Direction
 public static class LevelResources
 {
     #region Textures
-    public static Texture2D SpinnerBallExploded; // DONE
     public static Texture2D PipeHorizontal; // 0✔ 1X 2X 3X 4X
     public static Texture2D PipeHorizontalAlt; // 0X 1✔ 2X 3X 4X
     public static Texture2D PipeVertical; // 0✔ 1X 2X 3X 4X
@@ -216,7 +215,6 @@ public static class LevelResources
     // Don't worry, I'll get rid of this after I'm done with the conversion
     public static void LoadTextures()
     {
-        SpinnerBallExploded = Statics.Content.Load<Texture2D>("SpinnerBallExploded");
         PipeHorizontal = Statics.Content.Load<Texture2D>(@$"{Configs.GraphicSet}\PipeHorizontal");
         PipeVertical = Statics.Content.Load<Texture2D>(@$"{Configs.GraphicSet}\PipeVertical");
         PipeHorizontalAlt = Configs.GraphicSet is 1 ? PipeHorizontal : Statics.Content.Load<Texture2D>(@$"{Configs.GraphicSet}\PipeHorizontalAlt");
