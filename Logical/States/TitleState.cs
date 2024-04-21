76c535a37b6260e52562613c6ccf07a63866ae16
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-04-21 17:06:20 -0300

    perf(TitleState): Synchronous

--- Logical/States/TitleState.cs
+++ Logical/States/TitleState.cs
@@ -13,8 +13,13 @@ public class TitleState : GameState
     public TitleState(Game game) : base(game) => Statics.ShowCursor = false;
 
     #region Fields
+
+    private bool _isEnding;
     private Song _titel;
     private SimpleImage _background;
+    private const int MusicTransitionTime = 8000;
+    private const int SilenceTime = 8720;
+    private int _transitionCounter;
     #endregion
 
     #region Default Methods
@@ -28,7 +33,24 @@ public class TitleState : GameState
         MediaPlayer.MediaStateChanged += EndCaller;
     }
 
-    public override void Update(GameTime gameTime) {}
+    public override void Update(GameTime gameTime)
+    {
+        if (!_isEnding) return;
+        
+        if (_transitionCounter < MusicTransitionTime)
+            MediaPlayer.Volume = MathHelper.LerpPrecise(MathF.Pow(Configs.MusicVolume * 0.1f, 2), 0f, _transitionCounter / (float) MusicTransitionTime);
+        else
+        {
+            Statics.BackdropOpacity = 1;
+            MediaPlayer.Stop();
+            MediaPlayer.Volume = Configs.MusicVolume * 0.1f;
+        }
+        
+        _transitionCounter += gameTime.ElapsedGameTime.Milliseconds;
+        
+        if (_transitionCounter >= SilenceTime)
+            SwitchState(new MenuState(Game));
+    }
 
     protected override void UnloadContent()
     {
@@ -36,26 +58,19 @@ public class TitleState : GameState
         Game.Content.UnloadAsset("Credit");
     }
 
-    public override void HandleInput(object s, InputKeyEventArgs e) => EndScreen();
-    public override void HandleInput(object s, ButtonEventArgs e) => EndScreen();
+    public override void HandleInput(object s, InputKeyEventArgs e) => EndCaller(s, e);
+    public override void HandleInput(object s, ButtonEventArgs e) => EndCaller(s, e);
     #endregion
 
     #region Custom Methods
-    private void EndCaller(object s, EventArgs e) => EndScreen();
-    private async void EndScreen()
+    private void EndCaller(object s, object e)
     {
         Game.Window.KeyDown -= HandleInput;
         Input.ButtonDown -= HandleInput;
         MediaPlayer.MediaStateChanged -= EndCaller;
-        for (float i = 1; i > 0; i -= 0.0125f)
-        {
-            MediaPlayer.Volume = MathF.Pow(Configs.MusicVolume * 0.1f, 2) * i;
-            await Task.Delay(100);
-        }
-        Statics.BackdropOpacity = 1;
-        MediaPlayer.Stop(); MediaPlayer.Volume = Configs.MusicVolume * 0.1f;
-        await Task.Delay(720);
-        SwitchState(new MenuState(Game));
+        if (Configs.MusicVolume is 0)
+            _transitionCounter = MusicTransitionTime;
+        _isEnding = true;
     }
     #endregion
 }
