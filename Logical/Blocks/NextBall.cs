36dd0640738201f9b5ad572fa93c1816b04706c2
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-07-13 03:15:20 -0300

    feat(NextBall): fixable

--- Logical/Blocks/NextBall.cs
+++ Logical/Blocks/NextBall.cs
@@ -4,15 +4,16 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Logical.Blocks;
 
-public class NextBall : Block
+public class NextBall : Block, IFixable
 {
     #region Field
     private readonly Vector2 _holderOffset = new(9f);
-    private readonly Vector2 _indicatorOffset = new(12f);
     private readonly Vector2 _shadowOffset = new(12f, 13f);
     private static Texture2D _holder;
     private static Texture2D _indicators;
     private static Texture2D _shadow;
+    private Rectangle? _holderSource = new(0, 1, 18, 17);
+    private Vector2 _indicatorOffset = new(12f, 11f);
     #endregion
 
     public NextBall(Game game, Point arrayPosition, byte xx, byte yy)
@@ -34,7 +35,7 @@ public class NextBall : Block
         base.Draw(gameTime);
         
         DrawAnotherTexture(_shadow, _shadowOffset, 1);
-        DrawAnotherTexture(_holder, _holderOffset, 2);
+        DrawAnotherTexture(_holder, _holderOffset, 2, _holderSource);
         DrawAnotherTexture(_indicators, _indicatorOffset, 3, new Rectangle(12 * (int)LevelState.NextBall, 0, 12, 12));
     }
 
@@ -43,5 +44,13 @@ public class NextBall : Block
         _holder = _indicators = _shadow = null;
         Game.Content.UnloadAssets(new []{"Holder", "Indicators", $"{Configs.GraphicSet}/HolderShadowEmpty"});
         base.UnloadContent();
+    }
+
+    public IFixable.FidelityLevel Fidelity => IFixable.FidelityLevel.Intended;
+    
+    public void Fix(IFixable.FidelityLevel fidelity)
+    {
+        _holderSource = null;
+        ++_indicatorOffset.Y;
     }
 }
