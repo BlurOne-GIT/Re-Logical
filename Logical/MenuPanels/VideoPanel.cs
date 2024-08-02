337d4a8da1bef679d5bf0f17037bced1bce7e30d
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-08-02 20:29:38 -0300

    feat(Sprites/UI): window mode

--- Logical/MenuPanels/VideoPanel.cs
+++ Logical/MenuPanels/VideoPanel.cs
@@ -7,21 +7,22 @@ namespace Logical.MenuPanels;
 public class VideoPanel : MenuPanel
 {
     private readonly Button _scaleButton;
-    private readonly Button _fullScreenButton;
+    private readonly Button _windowModeButton;
     private readonly Button _backButton;
+    private static Rectangle WindowModeRectangle => new(0, Configs.Fullscreen ? 0 : 16, 103, 16);
     
     public VideoPanel(Game game) : base(game)
     {
         Components.Add(_scaleButton = new Button(Game, new Rectangle(108, 87,103, 16)));
         Components.Add(
-            _fullScreenButton = new Button(Game, new Rectangle(108, 109, 103, 16),
+            _windowModeButton = new Button(Game, new Rectangle(108, 109, 103, 16),
                 new SimpleImage(Game, $"{Configs.GraphicSet}/UI/WindowMode", new Vector2(108, 109), 3)
-                    { DefaultRectangle = new Rectangle(0, 0, 103, Configs.Fullscreen ? 16 : 0) }
+                    { DefaultRectangle = WindowModeRectangle }
             ));
         Components.Add(_backButton = new Button(Game, new Rectangle(108, 201, 103, 16)));
         
         _scaleButton.LeftClicked += Scale;
-        _fullScreenButton.LeftClicked += Fullscreen;
+        _windowModeButton.LeftClicked += Fullscreen;
         _backButton.LeftClicked += Back;
     }
     
@@ -36,7 +37,7 @@ public class VideoPanel : MenuPanel
     private void Fullscreen(object s, EventArgs e)
     {
         Configs.Fullscreen ^= true;
-        _fullScreenButton.Image.DefaultRectangle = new Rectangle(0, 0, 103, Configs.Fullscreen ? 16 : 0);
+        _windowModeButton.Image!.DefaultRectangle = WindowModeRectangle;
     }
     
     private void Back(object s, EventArgs e) => SwitchState(new SettingsPanel(Game));
@@ -50,7 +51,7 @@ public class VideoPanel : MenuPanel
     protected override void Dispose(bool disposing)
     {
         _scaleButton.LeftClicked -= Scale;
-        _fullScreenButton.LeftClicked -= Fullscreen;
+        _windowModeButton.LeftClicked -= Fullscreen;
         _backButton.LeftClicked -= Back;
 
         base.Dispose(disposing);
