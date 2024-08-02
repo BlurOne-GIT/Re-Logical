d0b460c4694d1c5d7c56779e1c680f34ccde1c89
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-08-01 23:45:41 -0300

    feat(Sprites/UI): settings panel

---
+++ Logical/MenuPanels/FixesPanel.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.Xna.Framework;
+using MmgEngine;
+
+namespace Logical.MenuPanels;
+
+public class FixesPanel : MenuPanel
+{
+    private readonly Button _backButton;
+    
+    public FixesPanel(Game game) : base(game)
+    {
+        Components.Add(_backButton = new Button(Game, new Rectangle(108, 201, 103, 16)));
+        _backButton.LeftClicked += Back;
+    }
+
+    private void Back(object s, EventArgs e) => SwitchState(new SettingsPanel(Game));
+    
+    protected override void Dispose(bool disposing)
+    {
+        _backButton.LeftClicked -= Back;
+
+        base.Dispose(disposing);
+    }
+}
--- Logical/MenuPanels/SettingsPanel.cs
+++ Logical/MenuPanels/SettingsPanel.cs
@@ -6,18 +6,37 @@ namespace Logical.MenuPanels;
 
 public class SettingsPanel : MenuPanel
 {
+    private readonly Button _videoButton;
+    private readonly Button _audioButton;
+    private readonly Button _fixesButton;
     private readonly Button _backButton;
     
     public SettingsPanel(Game game) : base(game)
     {
+        Components.Add(_videoButton = new Button(Game, new Rectangle(108, 87, 103, 16)));
+        Components.Add(_audioButton = new Button(Game, new Rectangle(108, 109, 103, 16)));
+        Components.Add(_fixesButton = new Button(Game, new Rectangle(108, 132, 103, 16)));
         Components.Add(_backButton = new Button(Game, new Rectangle(108, 201, 103, 16)));
+        
+        _videoButton.LeftClicked += Video;
+        _audioButton.LeftClicked += Audio;
+        _fixesButton.LeftClicked += Fixes;
         _backButton.LeftClicked += Back;
     }
 
+    private void Video(object s, EventArgs e) => SwitchState(new VideoPanel(Game));
+    
+    private void Audio(object s, EventArgs e) => SwitchState(new AudioPanel(Game));
+    
+    private void Fixes(object s, EventArgs e) => SwitchState(new FixesPanel(Game));
+    
     private void Back(object s, EventArgs e) => SwitchState(new MainPanel(Game));
 
     protected override void Dispose(bool disposing)
     {
+        _videoButton.LeftClicked -= Video;
+        _audioButton.LeftClicked -= Audio;
+        _fixesButton.LeftClicked -= Fixes;
         _backButton.LeftClicked -= Back;
         
         base.Dispose(disposing);
