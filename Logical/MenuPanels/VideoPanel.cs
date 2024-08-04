28c7dd28dd80032f062e4eafb11dc7d088963536
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-08-04 03:18:37 -0300

    chore(MmgEngine): pull develop

--- Logical/MenuPanels/VideoPanel.cs
+++ Logical/MenuPanels/VideoPanel.cs
@@ -6,23 +6,26 @@ namespace Logical.MenuPanels;
 
 public class VideoPanel : MenuPanel
 {
+    private readonly Button _windowModeButton;
+    private readonly SimpleImage _windowModeImage; 
+    private static Rectangle WindowModeRectangle => new(0, Configs.Fullscreen ? 0 : 16, 103, 16);
+    
     private readonly Button _scaleUpButton;
     private readonly TextComponent _scaleText;
     private readonly Button _scaleDownButton;
-    private readonly Button _windowModeButton;
+    
     private readonly Button _backButton;
-    private static Rectangle WindowModeRectangle => new(0, Configs.Fullscreen ? 0 : 16, 103, 16);
     
     public VideoPanel(Game game) : base(game)
     {
+        Components.Add(_windowModeButton = new Button(Game, new Rectangle(108, 109, 103, 16)));
+        Components.Add(_windowModeImage =
+            new SimpleImage(Game, $"{Configs.GraphicSet}/UI/WindowMode", new Vector2(108, 109), 3)
+            { DefaultRectangle = WindowModeRectangle }
+        );
         Components.Add(_scaleUpButton = new Button(Game, new Rectangle(158, 111,10, 10)));
         Components.Add(_scaleText = new TextComponent(Game, Statics.BoldFont, $"{Configs.Scale:00}", new Vector2(168, 112), 3));
         Components.Add(_scaleDownButton = new Button(Game, new Rectangle(184, 111, 10, 10)));
-        Components.Add(
-            _windowModeButton = new Button(Game, new Rectangle(108, 109, 103, 16),
-                new SimpleImage(Game, $"{Configs.GraphicSet}/UI/WindowMode", new Vector2(108, 109), 3)
-                    { DefaultRectangle = WindowModeRectangle }
-            ));
         Components.Add(_backButton = new Button(Game, new Rectangle(108, 201, 103, 16)));
         
         _scaleUpButton.LeftClicked += ScaleUp;
@@ -38,7 +41,7 @@ public class VideoPanel : MenuPanel
     private void Fullscreen(object s, EventArgs e)
     {
         Configs.Fullscreen ^= true;
-        _windowModeButton.Image!.DefaultRectangle = WindowModeRectangle;
+        _windowModeImage.DefaultRectangle = WindowModeRectangle;
     }
     
     private void Back(object s, EventArgs e) => SwitchState(new SettingsPanel(Game));
