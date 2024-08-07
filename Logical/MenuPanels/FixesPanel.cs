a0e22b06830ad9bf192012f6eaad5f182892a14c
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-08-07 17:54:08 -0300

    feat(Sprites/UI): fidelity levels

--- Logical/MenuPanels/FixesPanel.cs
+++ Logical/MenuPanels/FixesPanel.cs
@@ -6,18 +6,42 @@ namespace Logical.MenuPanels;
 
 public class FixesPanel : MenuPanel
 {
+    private readonly Button _fidelityLevelButton;
+    private readonly SimpleImage _fidelityLevelImage;
     private readonly Button _backButton;
+    
+    private static Rectangle FidelityLevelRectangle => new(0, (int)Configs.FidelityLevel * 23, 103, 23);
     
     public FixesPanel(Game game) : base(game)
     {
+        Components.Add(_fidelityLevelButton = new Button(Game, new Rectangle(108, 109, 103, 16)));
+        Components.Add(_fidelityLevelImage =
+            new SimpleImage(Game, $"{Configs.GraphicSet}/UI/FidelityLevels", new Vector2(108, 109), 3)
+            { DefaultRectangle = FidelityLevelRectangle }
+        );
         Components.Add(_backButton = new Button(Game, new Rectangle(108, 201, 103, 16)));
+        
+        _fidelityLevelButton.LeftClicked  += FidelityLevel;
+        _fidelityLevelButton.RightClicked += FidelityLevel;
         _backButton.LeftClicked += Back;
     }
 
     private void Back(object s, EventArgs e) => SwitchState(new SettingsPanel(Game));
+
+    private void FidelityLevel(object s, ButtonEventArgs e)
+    {
+        if (e.Button is "RightButton")
+            --Configs.FidelityLevel;
+        else
+            ++Configs.FidelityLevel;
+        
+        _fidelityLevelImage.DefaultRectangle = FidelityLevelRectangle;
+    }
     
     protected override void Dispose(bool disposing)
     {
+        _fidelityLevelButton.LeftClicked  -= FidelityLevel;
+        _fidelityLevelButton.RightClicked -= FidelityLevel;
         _backButton.LeftClicked -= Back;
 
         base.Dispose(disposing);
